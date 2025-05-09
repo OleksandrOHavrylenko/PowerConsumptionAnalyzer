fe10d2c6f53ab77b8a08dd4f18f7506473f8daff
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-05-09 11:59:13 +0200

    Initial commit.

---
+++ src/main/java/com/example/iot/PowerConsumptionAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerConsumptionAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerConsumptionAnalyzerApplication.class, args);
+    }
+
+}
