ec082834cfa89791c3d357339332852f2c8ad43c
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-05-11 19:37:11 +0200

    Added module power-consumption-anomalies-consumer.

---
+++ power-consumption-anomalies-consumer/src/main/java/com/example/iot/AnomaliesConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomaliesConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnomaliesConsumerApplication.class, args);
+    }
+
+}
